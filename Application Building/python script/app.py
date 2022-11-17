ea8a70be6b58d89431d8f6c6360112284bce3fff
Sandhiya-Sadhasivam <89004886+Sandhiya-Sadhasivam@users.noreply.github.com>
2022-11-17 17:40:17 +0530

    APPLICATION BUILDING UPDATED

--- Application Building/python script/app.py
+++ Application Building/python script/app.py
@@ -1,42 +1,38 @@
+from ctypes import _NamedFuncPointer
 import numpy as np
 import pickle
-import joblib
+import joblib 
 import matplotlib
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt 
 import time
 import pandas
-import os
-from flask import  flask, request, jsonify, render_template
-
-app=flask(__name__)
-model=pickle.load(open('xxxxxxxxxxxxxxxxxxxxxxxxxxxx'))
-scale=pickle.load(open('xxxxxxxxxxxxxxxxxxxxxxxxxxxx'))
-
-@app.route('/')#route to display the homepage
-def home():
-    return render_template('index.html')# rendering the homepage
-
-@app.route('\predict',methods=["POST","GET"]) # route to show the prediction in a web UI
+import os 
+from flask import Flask, request, jsonify, render_template
+app = Flask (_NamedFuncPointer)
+model = pickle.load(open('C:/Users/SmartbridgePC/Desktop/AIML/Guided projects/rainfall prediction/Rainfall.pkl', 'rb')) 
+scale = pickle.load(open('C:/Users/SmartbridgePC/Desktop/AIML/Guided projects/rainfall prediction/scale.pkl', 'rb'))
+@app.route('/')# route to display the home page 
+def home(): 
+return render_template('index.html') #rendering the home page
+@app.route('/predict', methods=["POST", "GET"])# route to show the predictions in a web UI
 def predict():
-    input_feature=[x for x in request.form.values() ]
-    features_values=[np.array(input_feature)]
-    names=[['Location','MinTemp','MaxTemp','Rainfall','WindGustSpeed','WindSpeed9am',
-            'WindSpeed3pm','Humidity9am','Humidity3pm',
-            'Pressure9am','Pressure3pm','Temp9am','Temp3pm','RainToday',
-            'WindGustDir','WindDir9am','WindDir3pm','year','month','day']]
-    data=pandas.DataFrame(features_values,columns=names)
-    data=scale.fit_transform(data)
-    data=pandas.DataFrame(data,columns=names)
-    # predictions Using the loaded model file
-    prediction=model.predict(data)
-    pred_prob=model.predict_proba(data)
-    print(prediction)
-    if prediction=="yes":
-        return render_template("chance.html")
-    else:
-        return render_template("nochance.html")
-    # showing the prediction results in a UI
-if __name__=="__main__":
-  home()
-  predict()
-  
+#reading the inputs given by the user
+input_feature=[x for x in request.form.values()]
+features_values [np.array(input_feature)] 
+names=[['Location', 'MinTemp', 'MaxTemp', 'Rainfall', 'WindGustSpeed',
+'WindSpeed9am', 'WindSpeed3pm, 'Humidity9am, 'Humidity3pm',
+'Pressure9am', 'Pressure3pm', 'Temp9am', 'Temp3pm', 'RainToday',
+'WindGustDir", "Wind Dir9am', 'WindDir3pm, 'year', 'month', 'day']]
+data = pandas.DataFrame(features_values,columns-names)
+data = scale.fit_transform(data)
+data = pandas.DataFrame(data, columns names)
+# predictions using the loaded model file
+prediction-model.predict(data) 
+pred_prob model.predict_proba (data)
+print(prediction)
+if prediction == "Yes": 
+return render_template("chance.html")
+else:
+return render_template("nochance.html") 
+#showing the prediction results in a UI
+if __name__=="____main__":
